eea6505b93ff0e10364dc39cda1501bb108bb758
GasanovShamil <gasanov.sh@gmail.com>
2018-03-01 01:05:40 +0100

    integration de angular material, ajout d'un router et home component avec les dossiers pour les models et services

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMaterialModule} from './app.material.module';
 
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { HomeComponent } from './components/home/home.component';
+import { UserService } from "./services/user.service";
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    AppMaterialModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
