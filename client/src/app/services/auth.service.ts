84d3b7c1a884d3afb3e252cb40f8fe06f0917e7c
GasanovShamil <gasanov.sh@gmail.com>
2018-07-02 17:37:06 +0200

    fix

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -1,43 +1,41 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpErrorResponse} from "@angular/common/http";
-import {JwtHelperService } from '@auth0/angular-jwt';
-import { Observable, AjaxError } from 'rxjs';
-import {catchError, retry, tap} from 'rxjs/operators';
-const config = require('../../../appconfig.json')
+//import {JwtHelperService } from '@auth0/angular-jwt';
+//const config = require('../../../appconfig.json')
 import { User } from "../models/user";
 
 @Injectable()
 export class AuthService {
-  private config: any = config;
-  private jwtHelper = new JwtHelperService();
+  // // private config: any = config;
+  // private jwtHelper = new JwtHelperService();
   constructor(private http: HttpClient) { }
 
-  isLogedIn(): boolean {
-    let user = localStorage.getItem('currentUser');
-    return (user)?true:false;
-  }
-
-  getCurrentUser(): User {
-    let user = new User();
-    user = JSON.parse(localStorage.getItem('currentUser'));
-    return user;
-  }
-
-  login(loginData : JSON) {
-    return this.http.post<any>(config.base_url+"login", loginData, {}).map(
-      token => {
-        let user = new User();
-        user = this.jwtHelper.decodeToken(token).user;
-        localStorage.setItem("access_token" , JSON.stringify(token));
-        localStorage.setItem("currentUser", JSON.stringify(user));
-      }
-    )
-  }
-
-  logout(){
-    localStorage.removeItem("access_token");
-    localStorage.removeItem("currentUser")
-  }
+  // isLogedIn(): boolean {
+  //   let user = localStorage.getItem('currentUser');
+  //   return (user)?true:false;
+  // }
+  //
+  // getCurrentUser(): User {
+  //   let user = new User();
+  //   user = JSON.parse(localStorage.getItem('currentUser'));
+  //   return user;
+  // }
+  //
+  // login(loginData : JSON) {
+  //   return this.http.post<any>("localhost:3000/login", loginData, {}).map(
+  //     token => {
+  //       let user = new User();
+  //       user = this.jwtHelper.decodeToken(token).user;
+  //       localStorage.setItem("access_token" , JSON.stringify(token));
+  //       localStorage.setItem("currentUser", JSON.stringify(user));
+  //     }
+  //   )
+  // }
+  //
+  // logout(){
+  //   localStorage.removeItem("access_token");
+  //   localStorage.removeItem("currentUser")
+  // }
 
 
 
